291890419b7729016708ae3c63002491806bc08b
coukeina <coukeina@gmail.com>
2026-04-30 12:17:28 +0200

    Initialisation du projet (Arborescence, structure, code initial)

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,3 +6,14 @@ cards.forEach((card) => {
         card.classList.add("active");
     });
 });
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        nav.style.background = "rgba(0,0,0,0.4)";
+        nav.style.backdropFilter = "blur(5px)";
+    } else {
+        nav.style.background = "transparent";
+    }
+});
